c55dc0a87095b4ec1b0f0b3728254ef056f244ff
Ewerton Azevedo <ewerton.azevedo@live.co.uk>
2020-05-11 13:59:21 +0200

    :bug: Include Update.js on dist folder

---
+++ dist/Update.js
@@ -0,0 +1,71 @@
+//Node Internal Modules
+const { exec } = require('child_process');
+const LogController = require('@11elements/js-logcontroller');
+
+let wasUpdated = async (intervalKey) => {
+    const info = {}
+    return new Promise( ( resolve, reject )=> {
+        try{
+            exec(`git rev-parse @`, ( err, stdout, stderr ) => {
+                if ( err ) {
+                    LogController.error( `Error trying to get status of local branch: ${ err }. Clearing Interval` );
+                    clearInterval(intervalKey);
+                    resolve( false );
+                }
+                if ( stdout ){
+                    info.local = stdout
+                    try{
+                        exec(`git rev-parse @{u}`, ( err, stdout, stderr ) => {
+                            if ( err ) {
+                                LogController.error( `Error trying to get status of remote branch: ${ err }` );
+                                resolve( false );
+                            }
+                            if ( stdout ){
+                                info.remote = stdout
+                                console.log('Has to be updated? ',info.local, info.remote)
+                                if(info.local !== info.remote) installUpdates()
+                            }
+                            else{
+                                LogController.error( `Error trying to get status of remote branch: ${ stderr }` );
+                            }
+                        })
+                    }
+                    catch( error ){
+                        LogController.error( `Unexpected error ${ error }` );
+                    }
+                }
+                else{
+                    LogController.error( `Error trying to get status of local branch: ${ stderr }` );
+                    resolve( false )
+                }
+            });
+        }
+        catch( error ){
+            LogController.error( `Unexpected error ${ error }` );
+            reject( false )
+        }
+    })
+}
+
+let installUpdates = async () => {
+    try{
+        exec(`git pull -X theirs origin master`, ( err, stdout, stderr ) => {
+            if ( err ) {
+                LogController.error( `Error trying to install update: ${ err }` );
+            }
+            if ( stdout ){
+                LogController.info( `updating: ${ stdout }` );
+            }
+            else{
+                LogController.error( `Error while trying to install the update: ${ stderr }` );
+            }
+        });
+    }
+    catch( error ){
+        LogController.error('Catch Block', error)
+        LogController.error( `Error trying to update: ${ error }` );
+    }
+}
+
+module.exports = {'check': (intervalKey) => wasUpdated(intervalKey)}
+
